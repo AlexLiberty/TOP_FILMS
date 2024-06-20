6ae6f67b691012181efe798b98fcaf2d8b8280a2
Alex Yelienok <aelenok@gmail.com>
2024-06-20 21:00:19 +0200

    правки

--- TOP_FILMS/Models/MovieContext.cs
+++ TOP_FILMS/Models/MovieContext.cs
@@ -7,6 +7,7 @@ namespace TOP_FILMS.Models
         public DbSet<Movie> Movies { get; set; }
         public MovieContext(DbContextOptions<MovieContext> options) : base(options) 
         {
+            Database.EnsureDeleted();
             if (Database.EnsureCreated())
             {
                 Movies?.Add(new Movie
