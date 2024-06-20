9164532c6678df8708e21c0053a0ade21c0875c9
Alex Yelienok <aelenok@gmail.com>
2024-06-20 20:00:00 +0200

    Add project files.

---
+++ TOP_FILMS/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace TOP_FILMS.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public string PosterUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ TOP_FILMS/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using TOP_FILMS.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<MovieContext>(options => options.UseSqlServer(connection));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
